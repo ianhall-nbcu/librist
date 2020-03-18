fde4818a94936c2cc10598f72d9f0d27de61d30f
Sergio Ammirata <sergio@ammirata.net>
2020-03-18 08:31:32 -0400

    librist 2.1.3 removes implicit peer creation so we have to add it explicitely now

--- src/rist2mpeg.c
+++ src/rist2mpeg.c
@@ -307,7 +307,7 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	for (size_t i = 1; i < OUTPUT_COUNT; i++) {
+	for (size_t i = 0; i < OUTPUT_COUNT; i++) {
 		if (addr[i] == NULL) {
 			continue;
 		}
