0c210ff102129a1ac61cafb510650257eaeecfdf
Sergio Ammirata <sergio@ammirata.net>
2020-04-11 12:03:03 -0400

    change rist_sender_encrypt_aes_set to rist_sender_encryption_aes_set

--- src/mpeg2rist.c
+++ src/mpeg2rist.c
@@ -353,7 +353,7 @@ int main(int argc, char *argv[])
 
 	if (shared_secret != NULL) {
 		int keysize =  encryption_type == 1 ? 128 : 256;
-		if (rist_sender_encrypt_aes_set(ctx, shared_secret, keysize) == -1) {
+		if (rist_sender_encryption_aes_set(ctx, shared_secret, keysize) == -1) {
 			fprintf(stderr, "Could not add enable encryption\n");
 			exit(1);
 		}
