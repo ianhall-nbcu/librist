d33afab3ad16fad5c10e9d80a93e8ea660110340
Gijs Peskens <gijsje@heteigenwijsje.nl>
2020-05-04 17:20:01 +0200

    fixup! Fix double free error

--- src/rist-common.c
+++ src/rist-common.c
@@ -2871,7 +2871,7 @@ void rist_receiver_destroy_local(struct rist_receiver *ctx)
 
 #ifdef __linux
 			if (peer->cryptoctx)
-				//free(peer->cryptoctx);
+				free(peer->cryptoctx);
 #endif
 
 			msg(ctx->id, 0, RIST_LOG_INFO, "[CLEANUP] Freeing up peer memory allocation\n");
@@ -3055,7 +3055,7 @@ void rist_sender_destroy_local(struct rist_sender *ctx)
 
 #ifdef __linux
 		if (peer->cryptoctx)
-			free(peer->cryptoctx);
+			//free(peer->cryptoctx);
 #endif
 		if (peer->url)
 			free(peer->url);
