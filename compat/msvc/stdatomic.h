da7e6d7dc6d36f51f161783ab8900e60f8328d57
Gijs Peskens <gijsje@heteigenwijsje.nl>
2020-05-20 21:59:24 +0200

    Fix build under VS x64-Debug

--- compat/msvc/stdatomic.h
+++ compat/msvc/stdatomic.h
@@ -43,10 +43,12 @@
 
 typedef volatile LONG  __declspec(align(32)) atomic_int;
 typedef volatile ULONG __declspec(align(32)) atomic_uint;
+typedef volatile ULONG __declspec(align(32)) atomic_ulong;
 
 typedef enum {
     memory_order_relaxed,
-    memory_order_acquire
+    memory_order_acquire,
+    memory_order_release
 } msvc_atomic_memory_order;
 
 #define atomic_init(p_a, v)           do { *(p_a) = (v); } while(0)
