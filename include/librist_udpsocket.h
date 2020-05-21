455e77578c0d1fa259a40e7fa8e8933a854525f8
Sergio Ammirata <sergio@ammirata.net>
2020-05-20 20:34:11 -0400

    Fix public API for udpsocket (windows)

--- include/librist_udpsocket.h
+++ include/librist_udpsocket.h
@@ -8,6 +8,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
+#include <librist.h>
 
 /* TODO: check errno on windows */
 #include <errno.h>
@@ -56,7 +57,7 @@ typedef struct udpsocket_url_param {
  * Returns: socket descriptor, -1 for error (errno is set)
  *
  */
-int udpsocket_open(uint16_t af);
+RIST_API int udpsocket_open(uint16_t af);
 
 /* Open a udp socket and binds it to local [host] + [port].
  *
@@ -66,27 +67,27 @@ int udpsocket_open(uint16_t af);
  * (In case of gai_error, -1 is returned, errno is not set, check gai_error)
  *
  */
-int udpsocket_open_bind(const char *host, uint16_t port, const char *mciface);
+RIST_API int udpsocket_open_bind(const char *host, uint16_t port, const char *mciface);
 
 /*
  * Explicitly set TX/RX buffer size for [sd] to [bufsize], in bytes.
  * Returns -1 on error, 0 on success.
  */
-int udpsocket_set_buffer_size(int sd, uint32_t bufsize);
+RIST_API int udpsocket_set_buffer_size(int sd, uint32_t bufsize);
 
 /*
  *
  * Retrieve current RX buffer size for [sd].
  * Returns 0 on error, current RX bufsize on success.
  */
-uint32_t udpsocket_get_buffer_size(int sd);
+RIST_API uint32_t udpsocket_get_buffer_size(int sd);
 
 /*
  * Explicitly set the mcast interface for the socket [sd] to [mciface] for address
  * family [family].
  * Returns 0 on success, -1 on error (errno is set accordingly).
  */
-int udpsocket_set_mcast_iface(int sd, const char *mciface, uint16_t family);
+RIST_API int udpsocket_set_mcast_iface(int sd, const char *mciface, uint16_t family);
 
 /* Open a udp socket and connect it to remote [host] + [port].
  *
@@ -96,16 +97,16 @@ int udpsocket_set_mcast_iface(int sd, const char *mciface, uint16_t family);
  * (In case of gai_error, -1 is returned, errno is not set, check gai_error)
  *
  */
-int udpsocket_open_connect(const char *host, uint16_t port, const char *mciface);
+RIST_API int udpsocket_open_connect(const char *host, uint16_t port, const char *mciface);
 
-int udpsocket_resolve_host(const char *host, uint16_t port, struct sockaddr *addr);
+RIST_API int udpsocket_resolve_host(const char *host, uint16_t port, struct sockaddr *addr);
 
-int udpsocket_send(int sd, const void *buf, size_t size);
-int udpsocket_sendto(int sd, const void *buf, size_t size, const char *host, uint16_t port);
-int udpsocket_recv(int sd, void *buf, size_t size);
-int udpsocket_close(int sd);
-int udpsocket_parse_url(char *url, char *address, int address_maxlen, uint16_t *port, int *local);
-int udpsocket_parse_url_parameters(const char *url, udpsocket_url_param_t *params,
+RIST_API int udpsocket_send(int sd, const void *buf, size_t size);
+RIST_API int udpsocket_sendto(int sd, const void *buf, size_t size, const char *host, uint16_t port);
+RIST_API int udpsocket_recv(int sd, void *buf, size_t size);
+RIST_API int udpsocket_close(int sd);
+RIST_API int udpsocket_parse_url(char *url, char *address, int address_maxlen, uint16_t *port, int *local);
+RIST_API int udpsocket_parse_url_parameters(const char *url, udpsocket_url_param_t *params,
          int max_params, uint32_t *clean_url_len);
 
 #endif /* ifndef UDPSOCKET_H */
