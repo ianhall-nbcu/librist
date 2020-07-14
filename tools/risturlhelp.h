f96f53e05ac9306d1f227e0873b0aaa16309ef69
Sergio Ammirata <sergio@ammirata.net>
2020-07-13 20:53:07 -0400

    Tools: Add help text for timing-mode parameter

--- tools/risturlhelp.h
+++ tools/risturlhelp.h
@@ -15,6 +15,7 @@ const char help_urlstr[] = "\nUsage: append to end of individual rist:// url(s) 
 "    param rtt-min=###  minimum expected rtt\n"
 "    param rtt-max=###  maximum expected rtt\n"
 "    param verbose-level=#  Disable -1; Error 3, Warning 4, Notice 5, Info 6, Debug 7, simulation/dry-run 100\n"
+"    param timing-mode=#  0 = RTP Timestamp (defaut); 1 = Arrival Time, 2 = RTP/RTCP Timestamp+NTP\n"
 "  Main and Advanced Profiles\n"
 "    param aes-type=#  128 = AES-128, 256 = AES-256 must have passphrase too\n"
 "    param secret=abcde  encryption passphrase\n"
