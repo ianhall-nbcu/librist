55a45fe0b9659e543e6a5c78e43caaf9efa6bdf2
Gijs Peskens <gijs@in2ip.nl>
2020-08-19 16:33:40 +0200

    Work around stats not being reported in simple profile
    For now we just output the data stats, RTCP stats can be fixed later

--- src/stats.c
+++ src/stats.c
@@ -138,6 +138,8 @@ void rist_receiver_flow_statistics(struct rist_receiver *ctx, struct rist_flow *
 	for (size_t i = 0; i < flow->peer_lst_len; i++)
 	{
 		struct rist_peer *peer = flow->peer_lst[i];
+		if (!peer->is_data)
+			peer = peer->peer_data;
 		uint32_t avg_rtt = (peer->eight_times_rtt / 8);
 		uint32_t bitrate;
 		size_t eight_times_bitrate;
